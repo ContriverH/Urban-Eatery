b3cd82308117457167276aca03c9c1d0cd2b2c10
Himanshu Pal <himanshu.p19@iiits.in>
2022-01-06 12:43:47 +0530

    adding changes

---
+++ src/fakeData/burger.js
@@ -0,0 +1,59 @@
+var burger = [
+    {
+        "id": "25",
+        "name": "Bob's Burger Barn",
+        "category": "burger",
+        "img": "https://i.imgur.com/WrJzPxP.png",
+        "description": "Pecorino smellY cheese melted cheese",
+        "story": "Gummi bears topping pie pastry bonbon cupcake caramels. Muffin muffin jujubes donut",
+        "price": 4.99
+    },
+    {
+        "id": "26",
+        "name": "Burger Bros Ahoy",
+        "category": "burger",
+        "img": "https://i.imgur.com/x7ydJJh.png",
+        "description": "Pecorino smellY cheese melted cheese",
+        "story": "Gummi bears topping pie pastry bonbon cupcake caramels. Muffin muffin jujubes donut",
+        "price": 5.99
+    },
+    {
+        "id": "27",
+        "name": "Bigger Better Burger Joint",
+        "category": "burger",
+        "img": "https://i.imgur.com/MGzTqhg.png",
+        "description": "Pecorino smellY cheese melted cheese",
+        "story": "Gummi bears topping pie pastry bonbon cupcake caramels. Muffin muffin jujubes donut",
+        "price": 4.59
+    },
+    {
+        "id": "28",
+        "name": "The Burger King",
+        "category": "burger",
+        "img": "https://i.imgur.com/A3SFVZB.png",
+        "description": "Pecorino smellY cheese melted cheese",
+        "story": "Gummi bears topping pie pastry bonbon cupcake caramels. Muffin muffin jujubes donut",
+        "price":5.99
+    },
+    {
+        "id": "29",
+        "name": "Buzz Burgers Chicken",
+        "category": "burger",
+        "img": "https://i.imgur.com/wJBDIXL.png",
+        "description": "Pecorino smellY cheese melted cheese",
+        "story": "Gummi bears topping pie pastry bonbon cupcake caramels. Muffin muffin jujubes donut",
+        "price": 4.99
+    },
+    {
+        "id": "30",
+        "name": "Patty Buns Burgers",
+        "category": "burger",
+        "img": "https://i.imgur.com/FTbZrCC.png",
+        "description": "Pecorino smellY cheese melted cheese",
+        "story": "Gummi bears topping pie pastry bonbon cupcake caramels. Muffin muffin jujubes donut",
+        "price": 5.99
+    },
+
+
+]
+export default burger;
